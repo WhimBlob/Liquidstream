2d870dbcef6e60a2fead24e8c21d65eeb57e723e
Ughetto Jules <ughetto.jules@gmail.com>
2021-05-03 21:45:15 +0200

    Restart node mongo

--- Liquidstreamback/app.js
+++ Liquidstreamback/app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv:WhimBlob:TheC@k3IsLiquid@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
