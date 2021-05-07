dd73daac93f2bbccd42b3b5ad3554c36577632ea
Ughetto Jules <ughetto.jules@gmail.com>
2021-05-07 09:26:46 +0200

    app switched to server to resolve unknown problem

--- back/auth/passport.js
+++ back/auth/passport.js
@@ -1,67 +1,67 @@
 const passport = require('passport');
-//     LocalStrategy = require('passport-local').Strategy,
-//     User = require('../models/Schema').User,
-//     nanoid = require('nanoid');
+    LocalStrategy = require('passport-local').Strategy,
+    User = require('../models/Schema').User,
+    nanoid = require('nanoid');
  
-// passport.serializeUser( (user, cb) => {
-//     cb(null, user);
-// });
+passport.serializeUser( (user, cb) => {
+    cb(null, user);
+});
  
-// passport.deserializeUser( (obj, cb) => {
-//     cb(null, obj);
-// });
+passport.deserializeUser( (obj, cb) => {
+    cb(null, obj);
+});
  
-// // Passport strategy for handling user registration
-// passport.use('localRegister', new LocalStrategy({
-//         usernameField: 'username',
-//         passwordField: 'password',
-//         passReqToCallback: true
-//     },
-//     (req, password, done) => {
-//         User.findOne({$or: [{username: req.body.username}]},  (err, user) => {
-//             if (err)
-//                 return done(err);
-//             if (user) {
-//                 if (user.username === req.body.username) {
-//                     req.flash('username', 'Username is already taken');
-//                 }
+// Passport strategy for handling user registration
+passport.use('localRegister', new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    (req, password, done) => {
+        User.findOne({$or: [{username: req.body.username}]},  (err, user) => {
+            if (err)
+                return done(err);
+            if (user) {
+                if (user.username === req.body.username) {
+                    req.flash('username', 'Username is already taken');
+                }
  
-//                 return done(null, false);
-//             } else {
-//                 let user = new User();
-//                 user.password = user.generateHash(password);
-//                 user.username = req.body.username;
-//                 user.stream_key = nanoid.generate();
-//                 user.save( (err) => {
-//                     if (err)
-//                         throw err;
-//                     return done(null, user);
-//                 });
-//             }
-//         });
-//     }));
+                return done(null, false);
+            } else {
+                let user = new User();
+                user.password = user.generateHash(password);
+                user.username = req.body.username;
+                user.stream_key = nanoid.generate();
+                user.save( (err) => {
+                    if (err)
+                        throw err;
+                    return done(null, user);
+                });
+            }
+        });
+    }));
  
-// // Passport strategy for authenticating users
-// passport.use('localLogin', new LocalStrategy({
-//         usernameField: 'username',
-//         passwordField: 'password',
-//         passReqToCallback: true
-//     },
-//     (req, username, password, done) => {
+// Passport strategy for authenticating users
+passport.use('localLogin', new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    (req, username, password, done) => {
  
-//         User.findOne({'username': username}, (err, user) => {
-//             if (err)
-//                 return done(err);
+        User.findOne({'username': username}, (err, user) => {
+            if (err)
+                return done(err);
  
-//             if (!user)
-//                 return done(null, false, req.flash('username', 'Username doesn\'t exist.'));
+            if (!user)
+                return done(null, false, req.flash('username', 'Username doesn\'t exist.'));
  
-//             if (!user.validPassword(password))
-//                 return done(null, false, req.flash('password', 'Oops! Wrong password.'));
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('password', 'Oops! Wrong password.'));
  
-//             return done(null, user);
-//         });
-//     }));
+            return done(null, user);
+        });
+    }));
  
  
 module.exports = passport;
--- back/config/media_server.js
+++ back/config/media_server.js
@@ -1,30 +1,30 @@
-// const User = require('../models/Schema').User;
+const User = require('../models/Schema').User;
 
-// const NodeMediaServer = require('node-media-server'),
-//     config = require('./default').rtmp_server;
+const NodeMediaServer = require('node-media-server'),
+    config = require('./default').rtmp_server;
  
-// nms = new NodeMediaServer(config);
+nms = new NodeMediaServer(config);
  
-// nms.on('prePublish', async (id, StreamPath, args) => {
-//     let stream_key = getStreamKeyFromStreamPath(StreamPath);
-//     console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+nms.on('prePublish', async (id, StreamPath, args) => {
+    let stream_key = getStreamKeyFromStreamPath(StreamPath);
+    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
 
-//     User.findOne({stream_key: stream_key}, (err, user) => {
-//       if (!err) {
-//           if (!user) {
-//               let session = nms.getSession(id);
-//               session.reject();
-//           } else {
-//               // do stuff
-//           }
-//       }
-//   });
-// });
+    User.findOne({stream_key: stream_key}, (err, user) => {
+      if (!err) {
+          if (!user) {
+              let session = nms.getSession(id);
+              session.reject();
+          } else {
+              // do stuff
+          }
+      }
+  });
+});
 
  
-// const getStreamKeyFromStreamPath = (path) => {
-//     let parts = path.split('/');
-//     return parts[parts.length - 1];
-// };
+const getStreamKeyFromStreamPath = (path) => {
+    let parts = path.split('/');
+    return parts[parts.length - 1];
+};
  
-// module.exports = nms;
+module.exports = nms;
